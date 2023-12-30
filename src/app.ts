91009a4275a0dc90d44d45b451c17e7c6ad1f219
SasangaME <madhumaledirisinghe@gmail.com>
2023-12-30 11:59:00 +0530

    migrated error handling to config

--- src/app.ts
+++ src/app.ts
@@ -13,13 +13,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send(`${message}`);
 });
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    console.error(err.message, err.stack);
-    res.status(statusCode).json({ 'message': err.message });
-    return;
-});
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
--- src/config/middleware.config.ts
+++ src/config/middleware.config.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import { router } from './route.config';
@@ -10,4 +10,16 @@ export function configMiddleware(app: express.Application): void {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use(router);
+
+    app.use(handleExceptions(app));
+
+}
+
+function handleExceptions(app: express.Application) {
+    return (err: any, req: Request, res: Response, next: NextFunction) => {
+        const statusCode = err.statusCode || 500;
+        console.error(err.message, err.stack);
+        res.status(statusCode).json({ 'message': err.message });
+        return;
+    }
 }
