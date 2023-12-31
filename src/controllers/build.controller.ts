793341d95d5240314d00692a3488677ace476070
SasangaME <madhumaledirisinghe@gmail.com>
2023-12-31 11:26:26 +0530

    sample hasRole middlware

--- src/controllers/build.controller.ts
+++ src/controllers/build.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import { getBuild as getBuildService } from '../services/build.service';
 import { BuildDto } from '../models/dtos/build.dto';
+import { hasRole, paramMiddleware } from '../middleware/demo.middleware';
 
-function getBuild(req: Request, res: Response, next: NextFunction) {
+async function getBuild(req: Request, res: Response, next: NextFunction) {
     const data: BuildDto = getBuildService();
     res.json(data);
 }
 
 export const buildRouter = Router();
-buildRouter.get('/', getBuild);
+buildRouter.get('/', hasRole, paramMiddleware('admin'), getBuild);
---
+++ src/middleware/demo.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+export async function hasRole(req: Request, res: Response, next: NextFunction) {
+    await next();
+}
+
+export function paramMiddleware(role: string) {
+    return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        const roleHeader = req.headers?.role || 'user';
+        if (role != roleHeader) {
+            const error: ErrorDto = {
+                message: 'user is not allowed for this action',
+                stackTrace: 'this is a sample field'
+            }
+            res.status(401).json(error);
+        } else {
+            await next();
+        }
+    };
+}
+
+type ErrorDto = {
+    message: string;
+    stackTrace: string;
+}
